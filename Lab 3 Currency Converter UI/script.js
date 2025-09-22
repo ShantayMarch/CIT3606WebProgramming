5c4f975655323786d53e409788c3021f432733bf
ShantayMarch <marchs@student.elms.edu>
2025-09-22 15:54:19 -0400

    Add currency converter UI and functionality for USD to JPY conversion

---
+++ Lab 3 Currency Converter UI/script.js
@@ -0,0 +1,19 @@
+    
+     
+
+         function USDtoYen()
+         {
+            USD = document.getElementById("Currency").value;
+            document.getElementById("Currency2").value = USD*147.67;
+            let Yen = document.getElementById("Currency2").value;
+            alert( USD * 147.67 )
+         }
+
+
+
+         
+        
+        
+
+
+     
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # CIT 3606 repo for Shantay March
 
-Use this template to make your repo holding all the labs for this class.
