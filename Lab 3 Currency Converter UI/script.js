ce798fb8519dad99c2c319deb0bb7a20d04e419b
ShantayMarch <marchs@student.elms.edu>
2025-09-22 19:10:47 -0400

    second button added so it goes both ways now

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -7,7 +7,12 @@
             document.getElementById("Currency2").value= (USD * 147.87).toFixed(2)
          }
          
-         
+         function YenToUSD()
+         {
+            Yen= document.getElementById("Currency2").value  
+            document.getElementById("Currency").value= (Yen*0.0068 ).toFixed(2)
+
+         }
 
 
 
