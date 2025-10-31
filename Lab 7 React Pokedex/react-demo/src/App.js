17297904341976ff48db827d6183fc7638859c80
ShantayMarch <marchs@student.elms.edu>
2025-10-31 02:39:26 -0400

    my bad

--- Lab 7 React Pokedex/react-demo/src/App.js
+++ Lab 7 React Pokedex/react-demo/src/App.js
@@ -1,34 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Pokemon(props) {
-  return ( 
-  <div className="card">
-  <img src={props.image} alt={props.name} style={{width: "100%"}}/>
-  <div className="container">
-    <h4><b>{props.name}</b></h4>
-    <p>{props.type} Type</p>
-  </div>
-</div>
+
+function Pokemon() {
+  return (
+    <div className="Pokemancard">
+
+      <div className="card">
+        <img src="727.png" alt="Incineroar" style={{ width: "100%" }} />
+        <div className="container">
+          <h4><b>Incineroar</b></h4>
+          <p>Fire/Dark Type</p>
+        </div>
+      </div>
+
+     
+      <div className="card">
+        <img src="993.png" alt="Iron Jugulis" style={{ width: "100%" }} />
+        <div className="container">
+          <h4><b>Iron Jugulis</b></h4>
+          <p>Dark/Flying Type</p>
+        </div>
+      </div>
+
+      
+      <div className="card">
+        <img src="790.png" alt="Cosmoem" style={{ width: "100%" }} />
+        <div className="container">
+          <h4><b>Cosmoem</b></h4>
+          <p>Psychic Type</p>
+        </div>
+      </div>
+    </div>
   );
 }
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Pokedex</h1>
-
-<div className="Pokemancard">
-
-  <Pokemon  name="Incineroar"  image ="727.png" type="Fire/Dark" />
-  <Pokemon name=" Iron Jugulis" image ="993.png" type="Dark/Flying" />
-  <Pokemon name= "Cosmoem" image ="790.png" type="Psychic" />
-
-</div>
-
+        <h1>Pokedex</h1>
+        <Pokemon />
       </header>
     </div>
   );
