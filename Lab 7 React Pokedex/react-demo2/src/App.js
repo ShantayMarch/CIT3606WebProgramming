d49e8c306b32c85232e686f9818f3ccf315a8722
ShantayMarch <marchs@student.elms.edu>
2025-11-05 23:34:34 -0500

    moved 1 to demo 2

--- Lab 7 React Pokedex/react-demo2/src/App.js
+++ Lab 7 React Pokedex/react-demo2/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Pokemon(props) {
+  return ( 
+  <div className="card">
+  <img src={props.image} alt={props.name} style={{width: "100%"}}/>
+  <div className="container">
+    <h4><b>{props.name}</b></h4>
+    <p>{props.type} Type</p>
+  </div>
+</div>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Pokedex</h1>
+
+<div className="Pokemancard">
+
+  <Pokemon  name="Incineroar"  image ="727.png" type="Fire/Dark" />
+  <Pokemon name=" Iron Jugulis" image ="993.png" type="Dark/Flying" />
+  <Pokemon name= "Cosmoem" image ="790.png" type="Psychic" />
+
+</div>
+
       </header>
     </div>
   );
